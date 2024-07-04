2a659919cba451a8c2fa6dd84227550285a68050
J_DDev <69683068+JayyDoesDev@users.noreply.github.com>
2024-07-04 02:22:28 -0400

    feat: getSubCommandByName method added

--- Common/Discord.ts
+++ Common/Discord.ts
@@ -2,10 +2,16 @@ import { Response, Request } from "express";
 import {
   APIInteractionResponseChannelMessageWithSource,
   ApplicationCommandOptionType,
-  Snowflake
+  APIApplicationCommandSubcommandOption,
+  Snowflake,
+  APIInteractionDataOptionBase,
+  APIBaseInteraction,
+  APIApplicationCommandInteractionDataSubcommandOption,
+  InteractionType
 } from "discord-api-types/v10";
 
-type ValueResponse<T> = Record<"value", T>;
+type ValueResponse<T> = Pick<APIInteractionDataOptionBase<ApplicationCommandOptionType, T>, "name" | "type" | "value">;
+type Combine<T, P> = T & P;
 export class Discord {
     #req: Request;
     #res: Response;
@@ -18,6 +24,14 @@ export class Discord {
       this.#res.send(options);
     }
 
+    public getSubCommandByName(name: string):  APIBaseInteraction<InteractionType.ApplicationCommand, Pick<Combine<APIApplicationCommandInteractionDataSubcommandOption, Record<"id", Snowflake>>, "id" | "name" | "options"| "type">> {
+        const subCommand: APIBaseInteraction<InteractionType.ApplicationCommand, Combine<APIApplicationCommandInteractionDataSubcommandOption, Record<"id", Snowflake>>> = this.#req.body;
+        for (const sub of subCommand.data.options) {
+          if (sub.name == name) {
+            return subCommand;
+          }
+        }
+    }
     public getString(name: string): ValueResponse<string> {
         return this._get<ValueResponse<string>>(name, ApplicationCommandOptionType.String);
     }
@@ -53,7 +67,7 @@ export class Discord {
         return this._get<ValueResponse<Snowflake>>(name, ApplicationCommandOptionType.Attachment)
     }
     private _get<T>(value: any, optionType: ApplicationCommandOptionType): T | any {
-        const options: { name: string, type: number, value: Snowflake }[] = this.#req.body;
+        const options: Pick<APIInteractionDataOptionBase<ApplicationCommandOptionType, Snowflake>, "name" | "type" | "value">[] = this.#req.body;
         for (const option of options) {
           if (option.name == value && option.type == optionType) {
             return option;
