befea1849bdc6c4550a2522cce488658e4f712c5
J_DDev <69683068+JayyDoesDev@users.noreply.github.com>
2024-07-27 22:34:46 -0400

    Update index.ts

--- Source/index.ts
+++ Source/index.ts
@@ -14,44 +14,44 @@ import {
 
 config();
 const ctx: Context = new Context();
-[ "Command" ].forEach(async (x) => {
-    await require(`../Handlers/${ x }`).default(ctx);
+["Command"].forEach(async (x) => {
+  await require(`../Handlers/${x}`).default(ctx);
 });
 
 process.on("unhandledRejection", (error) => {
-    console.error("Unhandled promise rejection:", error);
+  console.error("Unhandled promise rejection:", error);
 });
 
 process.on("uncaughtException", (error) => {
-    console.error("Uncaught exception:", error);
+  console.error("Uncaught exception:", error);
 });
 
 ctx.kotoba
-      .middleware(verifyKeyMiddleware(process.env.PUBLICKEY))
-      .createRequest({
-        method: "post",
-        route: "/interactions",
-        callback: (req, res) => {
-          const d = ctx.kotoba.setRequestAndResponse(req, res);
-          d.getBody("type")
-            .then((type: InteractionType) => {
-                d.getBody("data")
-                  .then((data: APIApplicationCommand) => {
-                    switch (type) {
-                      case InteractionType.Ping: {
-                        res.send({ type: InteractionResponseType.Pong });
-                      }
-                      break;
-                      case InteractionType.ApplicationCommand: {
-                        const command: Command<APIChatInputApplicationCommandInteraction | APIContextMenuInteraction | Discord> = ctx.interactions.get(data.name);
-                        command.on(ctx, new Discord(req, res));
-                      }
-                    }
-                  })
+  .middleware(verifyKeyMiddleware(process.env.PUBLICKEY))
+  .createRequest({
+    method: "post",
+    route: "/interactions",
+    callback: (req, res) => {
+      const d = ctx.kotoba.setRequestAndResponse(req, res);
+      d.getBody("type")
+        .then((type: InteractionType) => {
+          d.getBody("data")
+            .then((data: APIApplicationCommand) => {
+              switch (type) {
+                case InteractionType.Ping: {
+                  res.send({ type: InteractionResponseType.Pong });
+                }
+                  break;
+                case InteractionType.ApplicationCommand: {
+                  const command: Command<APIChatInputApplicationCommandInteraction | APIContextMenuInteraction | Discord> = ctx.interactions.get(data.name);
+                  command.on(ctx, new Discord(req, res));
+                }
+              }
             })
-        }
-      })
-      .startApp(`Listening to port ${ctx.kotoba.port}`);
+        })
+    }
+  })
+  .startApp(`Listening to port ${ctx.kotoba.port}`);
 
 (async () => {
   const url: string = await ngrok.connect({ addr: process.env.PORT, authtoken: process.env.NGROK });
